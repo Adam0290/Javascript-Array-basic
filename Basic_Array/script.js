da45eb6dfd973564b5af8306c8f28346c7f29123
Adam_Codes <76964302+Adam0290@users.noreply.github.com>
2021-04-21 16:48:34 +0100

    Add files via upload

---
+++ Basic_Array/script.js
@@ -0,0 +1,13 @@
+// Javascript Array //
+
+
+let arr = ["egg", "milk", "bread",];  // Arrays are wrapped in square brackets. [] //
+console.log(arr[0]); // Access the Array >>>> arr[0] >>>> displays milk on the console log // 
+// The Array index always starts from 0. //
+
+
+// Although its possible you can put numbers and Boolean in the above Array its not good practice. // 
+// Best practice is to separate them >>>> numbers Array >>>>> Boolean Array //
+
+// We put things inside loops so that they can be executed repeatedly//
+
